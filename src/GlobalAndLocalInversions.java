2e38d22c276feb5171e7256a3ca59eda63233c55
Dan Popescu <popescudand@gmail.com>
2020-04-12 18:55:24 +0300

    Solution #3: O(n)

--- src/GlobalAndLocalInversions.java
+++ src/GlobalAndLocalInversions.java
@@ -2,9 +2,8 @@ public class GlobalAndLocalInversions {
 
     public static boolean isIdealPermutation(int[] A) {
         for (int i = 0; i < A.length; i++) {
-            for (int j = i + 1; j < A.length; j++) {
-                if (A[i] > A[j] && j - i > 1) return false;
-            }
+            if (Math.abs(i - A[i]) > 1)
+                return false;
         }
         return true;
     }
