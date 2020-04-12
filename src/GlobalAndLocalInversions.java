e691074071dbcceab70f49da385087ef54c3e9c2
Dan Popescu <popescudand@gmail.com>
2020-04-12 18:53:27 +0300

    Solution #2: O(n^2)

--- src/GlobalAndLocalInversions.java
+++ src/GlobalAndLocalInversions.java
@@ -1,17 +1,11 @@
 public class GlobalAndLocalInversions {
 
     public static boolean isIdealPermutation(int[] A) {
-        int global = 0, local = 0;
         for (int i = 0; i < A.length; i++) {
             for (int j = i + 1; j < A.length; j++) {
-                if (A[i] > A[j]) global++;
+                if (A[i] > A[j] && j - i > 1) return false;
             }
         }
-
-        for (int i = 0; i < A.length - 1; i++) {
-            if (A[i] > A[i + 1]) local++;
-        }
-
-        return local == global;
+        return true;
     }
 }
