a5f9b369e43711e91f93d442f554a1c7ba690c7c
Dan Popescu <popescudand@gmail.com>
2020-06-05 08:59:29 +0300

    New solution MiddleOfTheLinkedList

--- src/MiddleOfTheLinkedList.java
+++ src/MiddleOfTheLinkedList.java
@@ -1,23 +1,14 @@
 public class MiddleOfTheLinkedList {
 
     public ListNode middleNode(ListNode head) {
-        if (head.next == null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
 
-        int size = getListSize(head);
-
-        ListNode it = head;
-        for (int i = 0; i < size / 2; i++) {
-            it = it.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
 
-        return it;
-    }
-
-    public int getListSize(ListNode head) {
-        int size = 0;
-        for (ListNode it = head; it != null; it = it.next) {
-            size++;
-        }
-        return size;
+        return slow;
     }
 }
